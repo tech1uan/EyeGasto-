d39e6ebf3c85af863fcadb887cad7403ac843a9d
Juan Miguel Austria <juanmiguelplaysmc@gmail.com>
2026-07-13 23:43:02 +0800

    fixed brevo

--- backend/services/mailer.js
+++ backend/services/mailer.js
@@ -1,4 +1,4 @@
-import brevo from '@getbrevo/brevo';
+import * as brevo from '@getbrevo/brevo';
 
 const apiInstance = new brevo.TransactionalEmailsApi();
 apiInstance.setApiKey(brevo.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
